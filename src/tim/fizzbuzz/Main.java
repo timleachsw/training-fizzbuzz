4f0aef1b2e00e1e37cc78daa7cbb036c314cda7c
Tim Leach <Tim.Leach@softwire.com>
2019-11-19 15:01:49 +0000

    Initial FizzBuzz working

---
+++ src/tim/fizzbuzz/Main.java
@@ -0,0 +1,17 @@
+package tim.fizzbuzz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            String toPrint = "";
+
+            // divisible by 3?
+            if (i % 3 == 0)
+                toPrint += "Fizz";
+
+            // divisible by 5?
+
+        }
+    }
+}
