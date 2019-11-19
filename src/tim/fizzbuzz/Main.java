8769750143a57b55248f20a1111d6e161ae4afd5
Tim Leach <Tim.Leach@softwire.com>
2019-11-19 15:12:14 +0000

    Bang and bong

--- src/tim/fizzbuzz/Main.java
+++ src/tim/fizzbuzz/Main.java
@@ -14,6 +14,14 @@ public class Main {
             if (i % 5 == 0)
                 toPrint += "Buzz";
 
+            // divisible by 7?
+            if (i % 7 == 0)
+                toPrint += "Bang";
+
+            // divisible by 11?
+            if (i % 11 == 0)
+                toPrint = "Bong";  // overwrite
+
             // neither?
             if (toPrint.equals(""))
                 toPrint = String.valueOf(i);
